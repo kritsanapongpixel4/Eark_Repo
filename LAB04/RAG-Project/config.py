73aa40e5fa00fb4b1681df5036536dff750a0b56
Skele_E@rk <9kritsanapong9@gmail.com>
2026-09-08 14:36:33 +0700

    update config

--- LAB04/RAG-Project/config.py
+++ LAB04/RAG-Project/config.py
@@ -80,7 +80,7 @@ LLM_PROVIDER = os.getenv("LLM_PROVIDER", "gemini")  # ตัวเลือก: 
 LLM_MODEL = os.getenv("LLM_MODEL", None)
 
 # API Keys (ใส่ Key ตรงนี้ได้เลย หรือจะใส่ผ่าน Environment Variable ก็ได้)
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AQ.Ab8RN6Iuhg_Nd7Dp8iPFKrZjKtc7L1BsLaizrsNV3XLen6Qm3w")      # Google Gemini API Key
+GEMINI_API_KEY = os.getenv("[ENCRYPTION_KEY]")      # Google Gemini API Key
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")      # ใส่ OpenAI API Key ที่นี่
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")          # ใส่ Groq API Key ที่นี่
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "")
